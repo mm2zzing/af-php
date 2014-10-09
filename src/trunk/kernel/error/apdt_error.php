977d91701049adaad2d326543af0a88e356974bf
Lee Hyeon-gi <maayalee@gmail.com>
2014-10-09 20:33:22 +0900

    Relocate modules

--- src/trunk/kernel/error/apdt_error.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Apdt_Error extends Exception {
-  public function get_log() {
-    $message = $this->getMessage();
-    $trace = $this->getTraceAsString();
-    return "{$message} $trace";
-  }
-}
