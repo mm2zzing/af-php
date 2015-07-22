0a69d3b0d3af44240b5eb36fc4c5b7543ae734e0
maayalee <maayalee@gmail.com>
2015-07-22 17:40:57 +0900

    Relocate

--- system/apdos/plugins/mvc/View.php
+++ system/apdos/plugins/mvc/View.php
@@ -4,12 +4,14 @@ namespace apdos\plugins\mvc;
 use apdos\kernel\actor\Component;
 use apdos\kernel\core\errors\Core_Error;
 use apdos\kernel\log\Logger;
+use apdos\plugins\config\Config;
 
 class View extends Component {
   public function load_view($view_path, $data = array()) {
     foreach ($data as $key=>$value) {
       $$key = $value;
     }
-    include_once($view_path . '.php');
+    $application_path = Config::get_instance()->get_application_path();
+    include_once($application_path . '/' . $view_path . '.php');
   }
 }
